8a109ac176c98d55bf35f9180ba041a1948adca4
Leng <110669856+xiaoshaziYA@users.noreply.github.com>
2025-04-06 17:27:40 +0800

    fix #72

--- v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinEntityLivingBase.java
+++ v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinEntityLivingBase.java
@@ -6,12 +6,15 @@ import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.DamageSource;
+import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.fpsmaster.features.impl.render.DamageIndicator;
+
 
 @Mixin(EntityLivingBase.class)
 public abstract class MixinEntityLivingBase extends MixinEntity {
