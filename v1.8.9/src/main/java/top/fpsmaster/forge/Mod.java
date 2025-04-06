5c6b7386b4d800e778f5f09762494dca499072a1
Leng <110669856+xiaoshaziYA@users.noreply.github.com>
2025-04-06 18:04:24 +0800

    强制渲染图标

--- v1.8.9/src/main/java/top/fpsmaster/forge/Mod.java
+++ v1.8.9/src/main/java/top/fpsmaster/forge/Mod.java
@@ -1,12 +1,28 @@
 package top.fpsmaster.forge;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.ModMetadata;
 import top.fpsmaster.FPSMaster;
 
-@net.minecraftforge.fml.common.Mod(modid = "fpsmaster", useMetadata=true)
+@Mod(
+    modid = "fpsmaster",
+    name = "FPSMaster", 
+    version = "v4",
+    useMetadata = true
+)
 public class Mod {
-    @net.minecraftforge.fml.common.Mod.EventHandler
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ModMetadata meta = event.getModMetadata();
+        meta.logoFile = "fpsmaster:textures/icon.png"; 
+        
+        System.out.println("[FPSMaster] 图标路径: " + meta.logoFile);
+    }
+
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new ForgeEventAPI());
         FPSMaster.INSTANCE.initialize();
