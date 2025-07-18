d4a2605127aea83c32e6ad141948bde3d02ca23c
SuperSkidder <SuperSkidder@qq.com>
2025-07-18 16:40:45 +0800

    feat: add autoplay and icon

--- shared/java/top/fpsmaster/features/impl/utility/AutoGG.java
+++ shared/java/top/fpsmaster/features/impl/utility/AutoGG.java
@@ -1,10 +1,10 @@
 package top.fpsmaster.features.impl.utility;
 
-import net.minecraft.util.StringUtils;
 import top.fpsmaster.event.Subscribe;
 import top.fpsmaster.event.events.EventPacket;
 import top.fpsmaster.features.manager.Category;
 import top.fpsmaster.features.manager.Module;
+import top.fpsmaster.features.settings.impl.BooleanSetting;
 import top.fpsmaster.features.settings.impl.ModeSetting;
 import top.fpsmaster.features.settings.impl.TextSetting;
 import top.fpsmaster.interfaces.ProviderManager;
@@ -12,30 +12,28 @@ import top.fpsmaster.modules.logger.ClientLogger;
 import top.fpsmaster.utils.Utility;
 
 public class AutoGG extends Module {
+    public BooleanSetting autoPlay = new BooleanSetting("AutoPlay", false);
     public TextSetting message = new TextSetting("Message", "gg");
     public ModeSetting servers = new ModeSetting("Servers", 0, "hypxiel");
+
     public AutoGG() {
         super("AutoGG", Category.Utility);
-        this.addSettings(message,servers);
+        this.addSettings(autoPlay, message, servers);
     }
+
     @Subscribe
     public void onPacket(EventPacket event) {
-        if(event == null) return;
-        // judge packet type
-        if(event.type == EventPacket.PacketType.RECEIVE && ProviderManager.packetChat.isPacket(event.packet)){
-            switch (servers.getValue()){
-                // hypxiel
+        if (event.type == EventPacket.PacketType.RECEIVE && ProviderManager.packetChat.isPacket(event.packet)) {
+            switch (servers.getValue()) {
                 case 0:
-                    // get chat message
                     String componentValue = ProviderManager.packetChat.getChatComponent(event.packet).toString();
                     ClientLogger.info(componentValue);
-                    // check message is game over
-                    String chatMessage = ProviderManager.packetChat.getUnformattedText(event.packet);
                     boolean hasPlayCommand = componentValue.contains("ClickEvent{action=RUN_COMMAND, value='/play ");
-                    boolean hasDiedInformation = StringUtils.stripControlCodes(chatMessage).startsWith("You died!");
-                    if (hasPlayCommand && !hasDiedInformation) {
-                        // send chat packet message
+                    if (hasPlayCommand) {
                         Utility.sendChatMessage("/ac " + message.getValue());
+                        if (autoPlay.getValue()){
+                            Utility.sendChatMessage(componentValue.substring(componentValue.indexOf("value='/play ") + 11, componentValue.indexOf("'}")));
+                        }
                     }
                     break;
                 default:
